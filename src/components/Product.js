a79add551e0abfd7a0d0de5cb5b08a392cd21718
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-08 23:14:23 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,16 @@
-
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Product extends React.Component {
+export default class Product extends React.Component {
   render() {
     return (
       <div className="product">
+        <h2>Name: {this.props.name}</h2>
         <ul>
-          <li>{ this.props.name }</li>
-          <li>{ this.props.producer }</li>
-          <li>{ this.props.hasWatermark }</li>
-          <li>{ this.props.color }</li>
-          <li>{ this.props.weight }</li>
+          <li>{this.props.producer}</li>
+          <li>{this.props.hasWatermark}</li>
+          <li>{this.props.color}</li>
+          <li>{this.props.weight}</li>
         </ul>
       </div>
     )
@@ -19,18 +18,18 @@ class Product extends React.Component {
 }
 
 Product.defaultProps = {
-  hasWatermark: false,
-}
+  hasWatermark: false
+};
 
 Product.propTypes = {
   name: PropTypes.string.isRequired,
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
   color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
-  weight: weightProp
-};
+  weight: weightAllowed
+}
 
-function weightProp(props, propName, componentName) {
+function weightAllowed(props, propName, componentName) {
   const value = props[propName];
   if (value === undefined) {
     return new Error(propName + 'cannot be undefined.  Validation failed.')
